55aa4fde7fbb710061abb76bcb7adfa24c4117a4
Thomas Wiesner <thomas@mietenkaufen.net>
2014-06-12 12:38:47 +0200

    fixed the landing page

--- examples/dings.js
+++ examples/dings.js
@@ -1,2 +1,17 @@
-var blubb = "123";
-blbla = blubb + 123123;
+myApp.directive('scrollOnClick', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, $elm, attrs) {
+            var idToScroll = attrs.href;
+            $elm.on('click', function() {
+                var $target;
+                if (idToScroll) {
+                    $target = $(idToScroll);
+                } else {
+                    $target = $elm;
+                }
+                $("body").animate({scrollTop: $target.offset().top}, "slow");
+            });
+        }
+    }
+});
