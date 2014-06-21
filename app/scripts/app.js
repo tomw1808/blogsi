6d7ccba0d74aae0d57087d7ee7edd89ae3104aba
Thomas Wiesner <thomas@mietenkaufen.net>
2014-06-21 12:01:59 +0200

    new links and updated GA script

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,32 +1,33 @@
 'use strict';
 
 angular
-  .module('blogsiApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
+    .module('blogsiApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
         'hljs',
         "blogsi.tomo.01",
         'ngDisqus'
-  ])
-  .config(function ($routeProvider, $locationProvider, $disqusProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    ])
+    .config(function ($routeProvider, $locationProvider, $disqusProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
         $locationProvider
             .hashPrefix('!');
         $disqusProvider.setShortname("blogsi");
-  }).run(["$location", "$rootScope", "$disqus", function($location, $rootScope, $disqus) {
+    }).run(["$location", "$rootScope", "$disqus" , "$window", function ($location, $rootScope, $disqus, $window) {
         $disqus.commit($location.path());
         // register listener to watch route changes
-        $rootScope.$on( "$locationChangeSuccess", function(event, next, current) {
+        $rootScope.$on("$locationChangeSuccess", function (event, next, current) {
             $disqus.commit($location.path());
+            ga('send', 'pageview', {'page': $location.path(), 'title': next});
 
         });
 
--- app/scripts/modules/tomo/app.js
+++ app/scripts/modules/tomo/app.js
@@ -3,18 +3,18 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('blogsi.tomo.01', [
-    'ngRoute',
-    'ngDisqus'
-]).
-    config(['$routeProvider', function($routeProvider) {
+        'ngRoute',
+        'ngDisqus'
+    ]).
+    config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/tomo/:name', {
-            templateUrl: function(name){
+            templateUrl: function (name) {
                 return 'scripts/modules/tomo/' + name.name + '.html';
             },
             controller: 'TomoCtl'
         });
     }])
-    .controller("TomoCtl", ['$scope', '$rootScope', function($scope, $rootScope) {
+    .controller("TomoCtl", ['$scope', '$rootScope', function ($scope, $rootScope) {
 
         $rootScope.discussion = true;
 
